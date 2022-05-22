4ed21959b329314cd7ba5c69b2a3455b7425a145
alexismillanao <alexis.millanao@hotmail.com>
2022-05-22 17:45:34 -0400

    3 files modified

--- App.tsx
+++ App.tsx
@@ -1,11 +1,20 @@
 import * as React from 'react';
+import { useState } from 'react';
 import './style.css';
 
-export default function App() {
+const Counter = ({ counter, increment }) => {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <div>{counter}</div>
+      <button onClick={increment}>aumentar conteo</button>
     </div>
   );
+};
+
+export default function App() {
+  const [counter, setCounter] = useState(0);
+  const increment = () => {
+    setCounter(counter + 1);
+  };
+  return <Counter counter={counter} increment={increment} />;
 }
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # react-ts-ktn8qh
 
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-ktn8qh)
+
+Contador
+
+> https://youtu.be/duh3uKn0qnU
